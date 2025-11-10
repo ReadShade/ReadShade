34042aa4181fae37d5bdf7b07a3e6043964cf38d
papadanku <pauldang2@gmail.com>
2025-11-09 21:04:42 -0700

    Change to Shibuya theme

--- source/conf.py
+++ source/conf.py
@@ -23,6 +23,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = "shibuya"
 html_title = "ReadShade"
 html_static_path = ['_static']
